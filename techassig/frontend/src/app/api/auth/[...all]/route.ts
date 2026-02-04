710864791608bb93dd3f7c66696cf748e6830f53
Younes Amajid <yamajid@e1r3p5.1337.ma>
2026-02-04 20:04:36 +0100

    fixed

--- techassig/frontend/src/app/api/auth/[...all]/route.ts
+++ techassig/frontend/src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const {GET} = toNextJsHandler(auth);
+export const { GET, POST } = toNextJsHandler(auth);
